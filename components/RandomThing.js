1b1a0fba679d607c2dd1002a82dbec82dac302b4
Jordan Whistler <whistlerjordan@gmail.com>
2022-03-17 14:12:09 -0400

    duh

--- components/RandomThing.js
+++ components/RandomThing.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
-const myFunc = axios.get(
-  "https://angry-goodall-44bfac.netlify.app/.netlify/functions/myFunction"
-);
+const myFunc = () =>
+  axios.get(
+    "https://angry-goodall-44bfac.netlify.app/.netlify/functions/myFunction"
+  );
 
 export default function RandomThing() {
   return (
